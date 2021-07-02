1eccd3c1baf09a46689fee6adaf2871c9dedafd4
nodiscc <nodiscc@gmail.com>
2021-07-02 19:03:16 +0200

    tools: readthedocs/sphinx: fix display of markdown tables - https://docs.readthedocs.io/en/latest/guides/reproducible-builds.html

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ html_show_copyright = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx_rtd_theme']
+extensions = ['recommonmark', 'sphinx_rtd_theme', 'sphinx_markdown_tables']
 
 # Only parse .md files
 source_suffix = ['.md']
